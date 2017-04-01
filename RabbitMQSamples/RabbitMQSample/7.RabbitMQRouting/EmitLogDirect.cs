dde867ad7b71b2fad3d516ca068fcecb64f85b0e
Karina Kuroda <Karina Kuroda>
2017-04-01 16:10:21 -0300

    first commit

---
+++ RabbitMQSamples/RabbitMQSample/7.RabbitMQRouting/EmitLogDirect.cs
@@ -0,0 +1,51 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7.RabbitMQRouting
+{
+    class EmitLogDirect
+    {
+        public static void Main(string[] args)
+        {
+            args = new string[2];
+            args[0] = "error";
+            args[1] = "info";
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare(exchange: "direct_logs", type: "direct");
+               
+                var message = "Hello World!";
+                var body = Encoding.UTF8.GetBytes(message);
+
+               
+                foreach (var severity in args)
+                {
+                    channel.BasicPublish(exchange: "direct_logs",
+                                       routingKey: severity,
+                                       basicProperties: null,
+                                       body: body);
+                }
+
+
+                Console.WriteLine(" [x] Sent {0}", message);
+
+            }
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+
+
+        }
+        private static string GetMessage(string[] args)
+        {
+            return ((args.Length > 0)
+                   ? string.Join(" ", args)
+                   : "info: Hello World!");
+        }
+    }
+}
---
+++ RabbitMQSamples/RabbitMQSample/RabbitMQSample/Send.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+using System.IO;
+
+namespace RabbitMQSample
+{
+    class Send
+    {
+        public static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost", Port=5672};
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                //channel.QueueDeclare(queue: "hello",
+                //                     durable: false,
+                //                     exclusive: false,
+                //                     autoDelete: false,
+                //                     arguments: null);
+                for (int i = 0; i <1; i++)
+                {
+                    var file = File.ReadAllBytes(@"C:\Users\kmidori\Desktop\RabbitMQSample\RabbitMQSample\RabbitMQSample\HTMLPage1.html");
+
+                    var body = file;//Encoding.UTF8.GetBytes(file);
+
+                    channel.BasicPublish(exchange: "",
+                                         routingKey: "rabbitteste",
+                                         basicProperties: null,
+                                         body: body);
+                    Console.WriteLine(" [x] Sent {0}", i);
+                }
+               
+            }
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
